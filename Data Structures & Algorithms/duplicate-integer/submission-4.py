a943ea5e8988054914833c1b310a29af2e0c6fcf
Hemanta Regmi <111817601+HemantaRegmi@users.noreply.github.com>
2026-07-26 19:38:10 -0500

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        tracker = []
+        for num in nums:
+            if num in tracker:
+                return True
+            else:
+                tracker.append(num)
+        return False
